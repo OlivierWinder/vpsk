9aad2eac48065b77ac80ad46743166fa2b9ba8aa
Olivier Winder <olivierwinder@gmail.com>
2023-07-21 12:21:45 +0200

    contact form

--- js/submit.js
+++ js/submit.js
@@ -3,20 +3,21 @@ const submitBtn = document.querySelector('.contact__form__submit')
 submitBtn.addEventListener('click', function(e){
     e.preventDefault();
     const form = document.querySelector('.contact__form')
-    const name = form.querySelector('#name').value;
-    const email = form.querySelector('#email').value;
-    const phone = form.querySelector('#phone').value;
-    const onderwerp = form.querySelector('#onderwerp').value;
-    const bericht = form.querySelector('#bericht').value;
+    let name = form.querySelector('#name').value;
+    let email = form.querySelector('#email').value;
+    let phone = form.querySelector('#phone').value;
+    let onderwerp = form.querySelector('#onderwerp').value;
+    let bericht = form.querySelector('#bericht').value;
+    let body = 'name: ' + name + '</br> email: ' + email + '</br> telefoonnummer: ' + phone + '</br> onderwerp: ' + onderwerp + '</br> bericht: ' + bericht;
 
 
     Email.send({
 
-        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
-        To : 'them@website.com',
-        From : "you@isp.com",
+        SecureToken : "f3c441b3-80d5-4701-adf3-752426311028",
+        To : 'olivierwinder@gmail.com',
+        From : "olivierwinder@gmail.com",
         Subject : "This is the subject",
-        Body : "And this is the body"
+        Body : body
 
     }).then(
         message => alert(message)
