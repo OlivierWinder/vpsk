89d6babbf711b0a2faea33b9a3bd20f909c02966
Olivier Winder <olivierwinder@gmail.com>
2023-07-21 13:29:41 +0200

    new form

--- js/submit.js
+++ js/submit.js
@@ -1,26 +1,24 @@
-const submitBtn = document.querySelector('.contact__form__submit')
+// const submitBtn = document.querySelector('.contact__form__submit')
 
-submitBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    const form = document.querySelector('.contact__form')
-    let name = form.querySelector('#name').value;
-    let email = form.querySelector('#email').value;
-    let phone = form.querySelector('#phone').value;
-    let onderwerp = form.querySelector('#onderwerp').value;
-    let bericht = form.querySelector('#bericht').value;
-    let body = 'name: ' + name + '</br> email: ' + email + '</br> telefoonnummer: ' + phone + '</br> onderwerp: ' + onderwerp + '</br> bericht: ' + bericht;
+// submitBtn.addEventListener('click', function(e){
+//     e.preventDefault();
+//     const form = document.querySelector('.contact__form')
+//     let name = form.querySelector('#name').value;
+//     let email = form.querySelector('#email').value;
+//     let phone = form.querySelector('#phone').value;
+//     let onderwerp = form.querySelector('#onderwerp').value;
+//     let bericht = form.querySelector('#bericht').value;
+//     let body = 'name: ' + name + '</br> email: ' + email + '</br> telefoonnummer: ' + phone + '</br> onderwerp: ' + onderwerp + '</br> bericht: ' + bericht;
 
 
-    Email.send({
+//         Email.send({
+//             SecureToken : "bc265bb5-a142-417b-b90c-427a875ef6e6",
+//             To : 'owfingerboarding@gmail.com',
+//             From : "olivierwinder@gmail.com",
+//             Subject : "This is the subject",
+//             Body : body
+//         }).then(
+//         message => alert(message)
+//         );
 
-        SecureToken : "f3c441b3-80d5-4701-adf3-752426311028",
-        To : 'olivierwinder@gmail.com',
-        From : "olivierwinder@gmail.com",
-        Subject : "This is the subject",
-        Body : body
-
-    }).then(
-        message => alert(message)
-    );
-
-})
+// })
